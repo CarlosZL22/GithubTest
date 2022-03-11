4e1f1be1ee36c6968d76a17112aac307217dfbd5
CarlosZL22 <72097472+CarlosZL22@users.noreply.github.com>
2022-03-11 10:01:26 -0500

    Quick Update
    This was a quick script on python to test Github Features

---
+++ Hello World.py
@@ -0,0 +1,11 @@
+import csv
+import numpy as np
+#THis is a test File
+print("Hello World")
+nums = [12,14,16,2,15,100];
+strs = ["milk","eggs","bread","oats","jelly","peanuts"];
+for i in strs:
+    print(i)
+for i in nums:
+    print(i)
+print(type(nums[0]))
